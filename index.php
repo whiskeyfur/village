89d368d10944df083398d40a67a38bf875eda59e
John Walker II <john.walker.ii@gmail.com>
2023-02-27 08:54:24 -0800

    Checkpoint

--- index.php
+++ index.php
@@ -8,11 +8,10 @@ Leaf\View::attach(\Leaf\Blade::class);
 app()->blade->configure("views/", "views/cache");
 
 db()->connection(new PDO("sqlite:" . __DIR__ . "/data/website.db"));
-cookie()->unsetAll();
+//cookie()->unsetAll();
 
-
-app()->get('/', 'Home\HomeController@index');
 if ($_SERVER["REMOTE_ADDR"] == "::1")
     app()->setErrorHandler(['\Leaf\Exception\General', 'defaultError']);
 
+app()->get('/', 'Home\HomeController@index');
 app()->run();
